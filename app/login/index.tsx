acbd6f9586515aee4961467f258243d1bc2dfecd
Wix-Code <ogbonna428alex@gmail.com>
2025-03-19 01:57:08 +0100

    push

--- app/login/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Login = () => {
-  return (
-    <view>Login</view>
-  )
-}
-
-export default Login
--- app/signIn.tsx
+++ app/signIn.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 export default function signIn() {
@@ -8,7 +8,19 @@ export default function signIn() {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>signIn</Text>
+        <Image source={img} />
+        <View style={styles.contain}>
+          <Text style={styles.text}>{`Get your groceries \nwith nectar`}</Text>
+          <Text style={styles.connect}>Or connect with social media</Text>
+          <TouchableOpacity style={styles.touch}>
+            <Image source={g} style={styles.icon} />
+            <Text style={styles.iconText}>Continue with Google</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tou}>
+            <Image source={f} style={styles.icon} />
+            <Text style={styles.iconText}>Continue with facebook</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   )
@@ -16,7 +28,51 @@ export default function signIn() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
+  },
+  contain: {
+    padding: 20,
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    gap: 20
+  },
+  text: {
+    fontSize: 26,
+    fontWeight: 'bold'
+  },
+  touch: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#5383EC',
+    borderRadius: 16,
+    paddingHorizontal: 30,
+    height: 67,
+    width: "100%",
+    gap: 50
+  },
+  icon: {
+
+  },
+  iconText: {
+    color: "#FFFFFF",
+    fontSize: 20
+  },
+  tou: {
+    backgroundColor: "#4A66AC",
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 16,
+    paddingHorizontal: 30,
+    height: 67,
+    width: "100%",
+    gap: 50
+  },
+  connect: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+    textAlign: "center",
+    color: "#828282"
   }
 })
